8dc0b9dc3a7bf34b90c5aa6d4dc9dc9c16db25cc
adarsh patil <patiladarsh98032321212@gmail.com>
2024-06-11 01:04:45 +0530

    other commit

--- Neural-Network/test_neural_network.py
+++ Neural-Network/test_neural_network.py
@@ -4,27 +4,26 @@ import matplotlib.pyplot as plt
 from simple_classification_neural_network import NeuralNetwork
 
 
-# Load the .mat file
-mat_data = scipy.io.loadmat('Neural-Network/data1.mat')
-# Access the variables in the .mat file
-X = mat_data['X']
-y = mat_data['y']
-x = X.T
-x = np.insert(x, 0, np.array(len(x[0])), axis=0)
+
 
 """ print(x.shape)
 print(y.shape)
 print(y) """
 def run():
-    # Load the .mat file
-    mat_data = scipy.io.loadmat('Neural-Network/data1.mat')
-    # Access the variables in the .mat file
-    X = mat_data['X']
-    y = mat_data['y']
+    # load data from .npy file
+    data = np.load('Neural-Network/data.npy', allow_pickle=True)
+    #print(data)
+
+    # Access the variables in the .npy file
+    X = data.item().get('X')
+    y = data.item().get('y')
     x = X.T
-    x = np.insert(x, 0, np.array(len(x[0])), axis=0)
+    x = np.insert(x, 0, np.ones(len(x[0])), axis=0)
+
+    #print(x)
+    #print(y)
     y = y.T
-    nn = NeuralNetwork(2, [400,25,1], 3, epochs=100, learning_rate=0.001, batch_size=2500, seed=0) 
+    nn = NeuralNetwork(2, [400,25,1], 2, epochs=100, learning_rate=0.01, batch_size=2500, seed=0) 
     w,cost = nn.train(x, y)
     #print(f"predicted output: {nn.predict(x)}")
     #print(f"binary prediction: {(nn.predict(x) > 0.5).astype(int)}")
@@ -38,6 +37,6 @@ def run():
     plt.ylabel('Cost')
     plt.title('Cost vs Epochs')
     plt.show()
-run()
 
+run()
 
